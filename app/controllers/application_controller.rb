634f5434612659d15e3eb6aeb3aea36d824318b3
Lex259606 <alexis.white1@kellogg.northwestern.edu>
2026-03-18 04:44:58 +0000

    Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
+  # This runs before every single page load
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    # Find the user whose ID is stored in the session cookie
+    @current_user = User.find_by({ "id" => session["user_id"] })
   end
 end
