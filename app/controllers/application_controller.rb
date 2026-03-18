6e797dc33efd4378d1ef3b05ffded3c2a39aaf48
Lex259606 <alexis.white1@kellogg.northwestern.edu>
2026-03-18 23:51:10 +0000

    NavBar 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base 
-def current_user
-  @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-end
+  # This is the "magic" line that makes @current_user available in your views
+  before_action :current_user
 
-def force_login
-  if current_user.nil?
-    redirect_to "/login", alert: "Please log in first!"
+  def current_user
+    # We use @current_user so the HTML file can "see" it
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def force_login
+    if current_user.nil?
+      # Use "notice" to match your application.html.erb flash check
+      redirect_to "/login", notice: "Please log in first!"
+    end
   end
 end
-end
-
