9f79fbc894b9c879991f573566fdb62a277d4071
Lex259606 <alexis.white1@kellogg.northwestern.edu>
2026-03-18 04:48:46 +0000

    Filtering View

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,10 +6,18 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    
+    # --- EDIT THIS LINE ---
+    # Only find entries for this place AND for the person currently logged in
+    if @current_user
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      @entries = [] # If not logged in, show nothing
+    end
   end
 
   def new
+    # This renders the form to create a new place
   end
 
   def create
@@ -20,3 +28,4 @@ class PlacesController < ApplicationController
   end
 
 end
+
