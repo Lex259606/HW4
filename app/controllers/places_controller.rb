b3a0890bf89e6e22becfc60998735da6b6857418
Lex259606 <alexis.white1@kellogg.northwestern.edu>
2026-03-19 00:16:25 +0000

    Places Controler

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,36 +1,38 @@
 class PlacesController < ApplicationController
-before_action :force_login
-  def index
-  # Filter by the logged-in user's ID
-  @places = Place.where({ "user_id" => session[:user_id] })
-end
+  before_action :force_login
 
+  def index
+    # Only show places belonging to the logged-in user
+    @places = Place.where({ "user_id" => session[:user_id] })
+  end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    
-    # --- EDIT THIS LINE ---
-    # Only find entries for this place AND for the person currently logged in
-    if @current_user
-      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+
+    # SECURITY: If the place doesn't belong to the user, kick them out
+    if @place.nil? || @place["user_id"] != session[:user_id]
+      flash["notice"] = "Access denied."
+      redirect_to "/places"
     else
-      @entries = [] # If not logged in, show nothing
+      # Only show entries for this place that belong to THIS user
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session[:user_id] })
     end
   end
 
   def new
-    # This renders the form to create a new place
+    # Renders new.html.erb
   end
 
- def create
-  @place = Place.new
-  @place["name"] = params["name"]
-  
-  # Assign the place to the person logged in
-  @place["user_id"] = session[:user_id]
-  
-  @place.save
-  redirect_to "/places"
-end
+  def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place["user_id"] = session[:user_id] # Tie it to the logged-in user
+    
+    if @place.save
+      redirect_to "/places"
+    else
+      render :new
+    end
+  end
 end
 
