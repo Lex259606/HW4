687f650ade842da86f2a4aacb4e9b18ba5832f59
Lex259606 <alexis.white1@kellogg.northwestern.edu>
2026-03-19 00:11:38 +0000

    What

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,16 +6,15 @@ before_action :force_login
 end
 
 
-  def show
-    @place = Place.find_by({ "id" => params["id"] })
-    
-    # --- EDIT THIS LINE ---
-    # Only find entries for this place AND for the person currently logged in
-    if @current_user
-      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
-    else
-      @entries = [] # If not logged in, show nothing
-    end
+def show
+  @place = Place.find_by({ "id" => params["id"] })
+  
+  # This version is safer and matches your index logic
+  @entries = Entry.where({ 
+    "place_id" => @place["id"], 
+    "user_id" => session["user_id"] 
+  })
+end
   end
 
   def new
