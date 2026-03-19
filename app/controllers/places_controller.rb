f190e79e78b42a1ecfc8e47d7c4e9486381ca9ba
Lex259606 <alexis.white1@kellogg.northwestern.edu>
2026-03-19 00:00:20 +0000

    Security

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,9 @@
 class PlacesController < ApplicationController
 before_action :force_login
   def index
-    @places = Place.all
-  end
+  # Filter by the logged-in user's ID
+  @places = Place.where({ "user_id" => session[:user_id] })
+end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
@@ -20,12 +21,15 @@ before_action :force_login
     # This renders the form to create a new place
   end
 
-  def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
-  end
-
+ def create
+  @place = Place.new
+  @place["name"] = params["name"]
+  
+  # Assign the place to the person logged in
+  @place["user_id"] = session[:user_id]
+  
+  @place.save
+  redirect_to "/places"
+end
 end
 
