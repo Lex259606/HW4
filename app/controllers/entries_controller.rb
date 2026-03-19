dfb7b7bc452bf76a9ae83d133586fdde560fd515
Lex259606 <alexis.white1@kellogg.northwestern.edu>
2026-03-19 00:03:58 +0000

    Trail 9000

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,9 @@
 class EntriesController < ApplicationController
-before_action :force_login
+  before_action :force_login
+
   def new
-    # This renders the form to create a new entry
+    @entry = Entry.new
+    @entry["place_id"] = params["place_id"]
   end
 
   def create
@@ -9,21 +11,28 @@ before_action :force_login
     @entry = Entry.new
 
     # 2. Extract data from the "entry" hash in params
+    # Using .dig or the nested hash helps prevent crashes if a key is missing
     @entry["title"] = params["entry"]["title"]
     @entry["description"] = params["entry"]["description"]
     @entry["occurred_on"] = params["entry"]["occurred_on"]
     @entry["place_id"] = params["entry"]["place_id"]
     
     # 3. Assign the entry to the person currently logged in
+    # We use @current_user.id because before_action :current_user sets this up
     @entry["user_id"] = @current_user.id 
     
     # 4. Attach the uploaded file using Active Storage
-    # This line connects the image file to the database record
-    @entry.uploaded_image.attach(params["entry"]["uploaded_image"])
+    # We wrap this in an 'if' so it doesn't crash if the user forgets a photo
+    if params["entry"]["uploaded_image"]
+      @entry.uploaded_image.attach(params["entry"]["uploaded_image"])
+    end
 
     # 5. Save and Redirect
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    if @entry.save
+      redirect_to "/places/#{@entry["place_id"]}", notice: "Entry was successfully created."
+    else
+      # If save fails (validation error), go back to the 'new' form
+      render :new, status: :unprocessable_entity
+    end
   end
-
 end
